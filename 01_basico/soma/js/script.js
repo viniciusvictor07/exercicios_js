44d2f10083ad1b6d0e1067eac41eff92bb7e0a68
Vinícius Victor <viniciusvictordev@gmail.com>
2026-03-10 21:19:49 -0300

    implementar soma interativa de números do usuário

---
+++ 01_basico/soma/js/script.js
@@ -0,0 +1,6 @@
+let num1 = prompt('Digite um valor: ');
+let num2 = prompt('Digite outro valor: ');
+num1 = Number(num1);
+num2 = Number(num2);
+let soma = num1 + num2;
+alert(`o resultado da soma foi ${soma}.`);
