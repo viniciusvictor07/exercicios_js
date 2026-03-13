f444a871a17fa5989cb3ae629031af42020e0d68
Vinícius Victor <viniciusvictordev@gmail.com>
2026-03-13 12:58:17 -0300

    removendo pasta compras e mantendo apenas times

--- 01_basico/compras/js/script.js
+++
@@ -1,9 +0,0 @@
-const time = prompt('Digite o nome do time: ').trim().toLowerCase();
-
-if (time === 'flamengo') {
-  console.log('O MELHOR DO MUNDO!');
-} else if (time === 'vasco') {
-  console.log('ESSE AI NÃO DÁ...');
-} else {
-  console.log('Time interessante, mas o meu é melhor.');
-}
