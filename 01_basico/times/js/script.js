4b579309d6ce282bd4b412e1855d11ea9d3725e2
Vinícius Victor <viniciusvictordev@gmail.com>
2026-03-13 12:48:55 -0300

    refactor: renomeando pasta de compras para times

---
+++ 01_basico/times/js/script.js
@@ -0,0 +1,9 @@
+const time = prompt('Digite o nome do time: ').trim().toLowerCase();
+
+if (time === 'flamengo') {
+  console.log('O MELHOR DO MUNDO!');
+} else if (time === 'vasco') {
+  console.log('ESSE AI NÃO DÁ...');
+} else {
+  console.log('Time interessante, mas o meu é melhor.');
+}
