082c28896a75a47a6b12e977e10b099dd0d5f7b4
viniciusvictor07 <srzeldris12@gmail.com>
2026-04-05 00:18:46 -0300

    feat: implementar localstorage e refatorar criação de elementos

--- 02_projetos_web/todo_list/assets/js/main.js
+++ 02_projetos_web/todo_list/assets/js/main.js
@@ -1,20 +1,29 @@
-const adicionarBtn = document.getElementById("adicionarBtn");
-const tarefaInput = document.getElementById("tarefaInput");
-const tarefaLista = document.getElementById("tarefaLista");
+function meuEscopo() {
+  const adicionarBtn = document.getElementById("adicionarBtn");
+  const tarefaInput = document.getElementById("tarefaInput");
+  const tarefaLista = document.getElementById("tarefaLista");
 
-function adicionarTarefa() {
-  // botões de excluir e marcar como feita serão colocados dentro dessa div para melhor organização
-  const botoesContainer = document.createElement("div");
+  function adicionarTarefa() {
+    const tarefaTexto = tarefaInput.value.trim();
+    if (tarefaTexto !== "") {
+      const novoItem = criarElemento(tarefaTexto);
+      tarefaLista.appendChild(novoItem);
 
-  const tarefaTexto = tarefaInput.value.trim();
-  if (tarefaTexto !== "") {
+      tarefaInput.value = "";
+      salvarTarefas();
+    }
+  }
+  function criarElemento(tarefaTexto) {
+    // botões excluirBnt e marcarFeitaBtn serão colocados dentro na div para melhor organização
     const tarefaItem = document.createElement("li");
     tarefaItem.textContent = tarefaTexto;
+    const botoesContainer = document.createElement("div");
 
     const excluirBtn = document.createElement("button");
     excluirBtn.textContent = "Excluir";
     excluirBtn.addEventListener("click", function () {
       tarefaItem.remove();
+      salvarTarefas();
     });
     botoesContainer.appendChild(excluirBtn);
 
@@ -26,15 +35,37 @@ function adicionarTarefa() {
     botoesContainer.appendChild(marcarFeitaBtn);
 
     tarefaItem.appendChild(botoesContainer);
-    tarefaLista.appendChild(tarefaItem);
-    tarefaInput.value = "";
+    return tarefaItem;
   }
+
+  adicionarBtn.addEventListener("click", adicionarTarefa);
+
+  tarefaInput.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+      adicionarTarefa();
+    }
+  });
+  // salvar as tarefas no localStorage
+  function salvarTarefas() {
+    const listaParaSalvar = [];
+    const tarefas = document.querySelectorAll("#tarefaLista li");
+
+    tarefas.forEach((item) => {
+      const tarefa = item.firstChild.textContent;
+      listaParaSalvar.push(tarefa);
+    });
+
+    localStorage.setItem("listaTarefas", JSON.stringify(listaParaSalvar));
+  }
+  function carregarTarefas() {
+    const tarefasSalvas = JSON.parse(localStorage.getItem("listaTarefas"));
+    if (tarefasSalvas) {
+      tarefasSalvas.forEach((tarefa) => {
+        const novoItem = criarElemento(tarefa);
+        tarefaLista.appendChild(novoItem);
+      });
+    }
+  }
+  carregarTarefas();
 }
-
-adicionarBtn.addEventListener("click", adicionarTarefa);
-
-tarefaInput.addEventListener("keydown", function (e) {
-  if (e.key === "Enter") {
-    adicionarTarefa();
-  }
-});
+meuEscopo();
